e6298087c9a8d7f213e1768ead900e19362d2ccd
RuslanGon <ruslan441006@gmail.com>
2025-05-01 17:18:41 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-red-600 underline">
-    Hello world!
-  </h1>
+    <>
+    <Navbar />
+    </>
   )
 }
 
