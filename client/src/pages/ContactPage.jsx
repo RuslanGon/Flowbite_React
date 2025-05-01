11e9e5dc114f4550c0359d96c12afc45852419c8
RuslanGon <ruslan441006@gmail.com>
2025-05-01 18:40:12 +0300

    wer

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Datepicker } from 'flowbite-datepicker';
 
 const ContactPage = () => {
-  return (
-    <div>ContactPage</div>
-  )
-}
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const datepickerElement = document.getElementById('datepicker-container');
+      if (datepickerElement && !datepickerElement.querySelector('.datepicker')) {
+        new Datepicker(datepickerElement, {
+          inline: true,
+        });
+      }
+    }
+  }, []);
 
-export default ContactPage
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      {/* Контейнер для календаря */}
+      <div id="datepicker-container" className="relative max-w-sm"></div>
+    </div>
+  );
+};
+
+export default ContactPage;
